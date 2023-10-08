ca12df428428cfb60efaf85139aaaf30eeda20bb
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-10-08 22:18:54 +0500

    learned something new

--- NodeJs/Learning express/index.js
+++ NodeJs/Learning express/index.js
@@ -3,7 +3,17 @@ const path = require('path');
 const app = express();
 
 const dir = path.join(__dirname,'public');
-console.log(dir);
-app.use(express.static(dir));
+// console.log(dir);
+// app.use(express.static(dir));
+
+app.get('',(req, res)=>{
+    res.sendFile(dir+"/index.html")
+})
+app.get('/about',(req, res)=>{
+    res.sendFile(dir+"/about.html")
+})
+app.get('/home',(req, res)=>{
+    res.sendFile(dir+"/home.html")
+})
 
 app.listen(8000);
