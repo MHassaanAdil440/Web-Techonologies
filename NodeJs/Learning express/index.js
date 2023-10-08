3b3b744cc1a08a6c0ddebb7977e979b22d29b518
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-10-08 21:57:23 +0500

    used use middle ware with express.static to learn to use static html pages with node also used path

--- NodeJs/Learning express/index.js
+++ NodeJs/Learning express/index.js
@@ -1,39 +1,9 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
 
-function myHtml(currentRoute){
+const dir = path.join(__dirname,'public');
+console.log(dir);
+app.use(express.static(dir));
 
-    const html =
-    `<!DOCTYPE html>
-    <html>
-    <head>
-    <title>Hello World Example</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    </head>
-      <body>
-      <h1 class="display-4 text-primary">${currentRoute}</h1>
-      <br>
-      <a href="/" class="btn btn-success">Root</a>
-      <a href="/about" class="btn btn-success">About us</a>
-      <a href="/home" class="btn btn-info">Home</a>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-      </body>
-      </html>
-      `;
-      return html;
-}
-
-
-app.get("/", (req, res) => {
-    const currentRoute = req.path;
-      res.send(myHtml(currentRoute));
-});
-app.get("/about", (req, res) => {
-    const currentRoute = req.path;
-      res.send(myHtml(currentRoute));
-});
-app.get("/home", (req, res) => {
-    const currentRoute = req.path;
-      res.send(myHtml(currentRoute));
-});
-app.listen(5000);
+app.listen(8000);
