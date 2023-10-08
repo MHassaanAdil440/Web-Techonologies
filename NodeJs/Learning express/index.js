5bb01164ee56c5a921c0a90124e4e00ffa6ee9cf
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-10-08 20:46:51 +0500

    routes in express

--- NodeJs/Learning express/index.js
+++ NodeJs/Learning express/index.js
@@ -1,81 +1,39 @@
-// const express = require("express");
-// const app = express();
-
-
-// const html =
-//   `<!DOCTYPE html>
-// <html>
-// <head>
-// <title>Hello World Example</title>
-//       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-//       </head>
-//       <body>
-//       <h1 class="display-4 text-primary">` +
-//   name +
-//   `</h1>
-//       <br>
-//       <a href="/about" class="btn btn-success">About us</a>
-//       <a href="/home" class="btn btn-info">Home</a>
-      
-//       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-//       </body>
-//       </html>
-      
-//       `;
-// // app.get("/", (req, res) => {
-// //     const name = request.query.name;
-// //   res.send(name);
-// // });
-// app.get("/home", (request, response) => {
-//   response.send(html);
-// });
-// app.get("/about", (request, response) => {
-//   response.send(html);
-// });
-
-// app.listen(5000);
 const express = require("express");
 const app = express();
 
-app.get("/", (request, response) => {
-  // Get the "name" query parameter from the request
-  const name = request.query.name || "Guest"; // Default to "Guest" if the name parameter is not provided
+function myHtml(currentRoute){
 
-  // HTML content with Bootstrap styling
-  const html = `
-    <!DOCTYPE html>
+    const html =
+    `<!DOCTYPE html>
     <html>
     <head>
-        <title>Hello World Example</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <title>Hello World Example</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     </head>
-    <body>
-        <h1 class="display-4 text-primary">Hello, ${name}!</h1>
-        <br>
-        <a href="/about" class="btn btn-success">About us</a>
-        <a href="/home" class="btn btn-info">Home</a>
-        <a href="/hassaan" class="btn btn-primary">Hassaan</a>
+      <body>
+      <h1 class="display-4 text-primary">${currentRoute}</h1>
+      <br>
+      <a href="/" class="btn btn-success">Root</a>
+      <a href="/about" class="btn btn-success">About us</a>
+      <a href="/home" class="btn btn-info">Home</a>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+      </body>
+      </html>
+      `;
+      return html;
+}
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    </body>
-    </html>
-  `;
 
-  response.send(html);
+app.get("/", (req, res) => {
+    const currentRoute = req.path;
+      res.send(myHtml(currentRoute));
 });
-
-app.get("/home", (request, response) => {
-  response.redirect("/?name=Home");
+app.get("/about", (req, res) => {
+    const currentRoute = req.path;
+      res.send(myHtml(currentRoute));
 });
-
-app.get("/about", (request, response) => {
-  response.redirect("/?name=About");
+app.get("/home", (req, res) => {
+    const currentRoute = req.path;
+      res.send(myHtml(currentRoute));
 });
-
-app.get("/hassaan",(request, response)=>{
-    response.redirect("/?name=hassaan");
-});
-
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
+app.listen(5000);
