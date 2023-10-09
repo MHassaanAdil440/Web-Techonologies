fa11fecb100d93dc4679f972f15cb5c423ce2b66
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-10-09 15:13:14 +0500

    static data api

---
+++ NodeJs/data.js
@@ -0,0 +1,11 @@
+module.exports.data = [
+    {name:'Hassaan', email:'hassaan@test.com'},
+    {name:'Dracule Mihawk', email:"hawkeye@onepiece.com"},
+    {name:'Minato Namikaze', email:'yellowflashofkonoha@naruto.com'},
+    {name:'Kakashi Hatake', email:'copyninja@naruto.com'},
+    {name:'Obito Uchiha', email:'tobi@narutoShinpudin.com'},
+    {name:'Might Guy', email:'theRedBeast@narutoShinpudin.com'},
+    {name:'Madara Uchiha', email:'GhostofUchiha@narutoShinpudin.com'},
+    {name:'Itachi Uchiha', email:'UchihaItachi@narutoShinpudin.com'}
+];
+// module.exports.data = data;
