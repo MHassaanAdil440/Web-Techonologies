b7732418ec7ebc3eb1b4350d923b9dffb690c131
unknown <Hassaanadil56@gmail.com>
2023-12-16 14:11:55 +0500

    Login component added

--- Term Project/frontend/src/App.js
+++ Term Project/frontend/src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
           <Route path='/update' element={<h1>Update Product</h1>}/>
           <Route path='/logout' element={<h1>logout</h1>}/>
           <Route path='/profile' element={<h1>Profile</h1>}/>
+
           <Route path='/signup' element={<Signup/>}/>
+          <Route path='/login' element={<Login/>}/>
         </Routes>
       </BrowserRouter>
       <Footer/>
